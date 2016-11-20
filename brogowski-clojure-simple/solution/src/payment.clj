cabd51880dfc72bfc1c9ebe2443661b1243d2be1
Bartłomiej Rogowski <Bartomiej Rogowski>
2016-11-20 16:37:23 +0100

    Add support for duplicate packaging slip when product is a book

--- brogowski-clojure-simple/solution/src/payment.clj
+++ brogowski-clojure-simple/solution/src/payment.clj
@@ -1,17 +1,28 @@
 (ns solution.payment)
 
 (declare generate-packing-slip-stub)
+(declare generate-duplicate-packaging-slip-stub)
+
+(defn product-has-type?
+  [product type]
+  (let [types (:types product)]
+    (some #{type} types)))
 
 (defn recieve-payment
   ([payment]
-    (recieve-payment payment generate-packing-slip-stub))
-  ([payment generate-packing-slip]
+    (recieve-payment payment generate-packing-slip-stub generate-duplicate-packaging-slip-stub))
+  ([payment generate-packing-slip generate-duplicate-packaging-slip]
    (let
-    [product (:product payment)
-     type (:type product)]
-    (when (= type :physical)
-      (generate-packing-slip product)))))
+    [product (:product payment)]
+    (when (product-has-type? product :physical)
+      (generate-packing-slip product))
+    (when (product-has-type? product :book)
+      (generate-duplicate-packaging-slip product)))))
 
 (defn generate-packing-slip-stub
-  "Sub function for packagin slip generation/save/send."
+  "Stub function for packaging slip generation/save/send."
+  []())
+
+(defn generate-duplicate-packaging-slip-stub
+  "Stub function for duplicate packaging slip generation/save/send."
   []())
