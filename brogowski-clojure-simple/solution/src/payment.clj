93ed8fa4963a8e3cef6948237029c72a65c86d72
Bartłomiej Rogowski <Bartomiej Rogowski>
2016-11-20 17:56:13 +0100

    Refactor for more business rules to come

--- brogowski-clojure-simple/solution/src/payment.clj
+++ brogowski-clojure-simple/solution/src/payment.clj
@@ -1,28 +1,40 @@
 (ns solution.payment)
 
-(declare generate-packing-slip-stub)
-(declare generate-duplicate-packaging-slip-stub)
-
 (defn product-has-type?
   [product type]
   (let [types (:types product)]
     (some #{type} types)))
 
-(defn recieve-payment
-  ([payment]
-    (recieve-payment payment generate-packing-slip-stub generate-duplicate-packaging-slip-stub))
-  ([payment generate-packing-slip generate-duplicate-packaging-slip]
-   (let
-    [product (:product payment)]
-    (when (product-has-type? product :physical)
-      (generate-packing-slip product))
-    (when (product-has-type? product :book)
-      (generate-duplicate-packaging-slip product)))))
+(defn build-payment-rule
+  [activation-check activator]
+  {:activation-check (fn [product] (activation-check product))
+   :activator        (fn [product] (activator product))})
 
 (defn generate-packing-slip-stub
   "Stub function for packaging slip generation/save/send."
-  []())
+  [product](println (str "Packaging slip for: " product)))
 
 (defn generate-duplicate-packaging-slip-stub
   "Stub function for duplicate packaging slip generation/save/send."
-  []())
+  [product](println (str "Duplicate packaging slip for: " product)))
+
+(def payment-rules
+  (list (build-payment-rule #(product-has-type? % :physical)
+                            #(generate-packing-slip-stub %))
+        (build-payment-rule #(product-has-type? % :book)
+                            #(generate-duplicate-packaging-slip-stub %))))
+
+(defn execute-rule-if-applies
+  [product rule]
+  (let [rule-applies? (:activation-check rule)
+        execute-rule (:activator rule)]
+    (when (rule-applies? product)
+      (execute-rule product))))
+
+(defn recieve-payment
+  "Handles new payment"
+  ([payment]
+    (recieve-payment payment payment-rules))
+  ([payment rules]
+    (let [product (:product payment)]
+      (doall (map #(execute-rule-if-applies product %) rules)))))
